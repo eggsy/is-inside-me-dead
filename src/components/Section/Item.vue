86492c715e5748a03e524c8c27402fb3615c217a
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-02 19:57:41 +0300

    refactor: add animations to section items

--- src/components/Section/Item.vue
+++ src/components/Section/Item.vue
@@ -1,5 +1,19 @@
 <template>
-  <section class="space-y-4">
+  <section
+    v-motion
+    :initial="{
+      opacity: 0,
+      translateX: '25%',
+    }"
+    :visible="{
+      opacity: 1,
+      translateX: '0%',
+      transition: {
+      },
+    }"
+    :delay="500"
+    class="space-y-4"
+  >
     <slot />
   </section>
 </template>
