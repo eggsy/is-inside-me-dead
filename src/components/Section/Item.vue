d2498688273b77cf58c430c45832d66c172b286e
Abdulbaki Dursun <eggsydev@gmail.com>
2022-09-06 21:30:07 +0300

    feat: use visible-once to play animation only once

--- src/components/Section/Item.vue
+++ src/components/Section/Item.vue
@@ -1,19 +1,26 @@
+<script lang="ts" setup>
+import { useMotion } from "@vueuse/motion";
+import { ref } from "vue";
+
+const target = ref<HTMLElement>();
+
+useMotion(target, {
+  initial: {
+    opacity: 0,
+    translateX: "25%",
+  },
+  visibleOnce: {
+    opacity: 1,
+    translateX: "0%",
+    transition: {
+      delay: 500,
+    },
+  },
+});
+</script>
+
 <template>
-  <section
-    v-motion
-    :initial="{
-      opacity: 0,
-      translateX: '25%',
-    }"
-    :visible="{
-      opacity: 1,
-      translateX: '0%',
-      transition: {
-      },
-    }"
-    :delay="500"
-    class="space-y-4"
-  >
+  <section ref="target" :delay="500" class="space-y-4">
     <slot />
   </section>
 </template>
