aef9ae187a230ed5839ae9a97a07a7a77e91817a
Abdulbaki Dursun <eggsydev@gmail.com>
2022-09-06 21:05:12 +0300

    chore: fix typo

--- src/components/Footer/Main.vue
+++ src/components/Footer/Main.vue
@@ -1,21 +1,24 @@
 <script lang="ts" setup>
 // Typography
-import Title from "../Title.vue"
-import Text from "../Text.vue"
+import Title from "../Title.vue";
+import Text from "../Text.vue";
 
 // Layout
-import FooterContainer from "./Container.vue"
-import FooterItem from "./Item.vue"
+import FooterContainer from "./Container.vue";
+import FooterItem from "./Item.vue";
 
 // Elements
-import FooterSocials from "./Socials.vue"
+import FooterSocials from "./Socials.vue";
 </script>
 
 <template>
   <FooterContainer>
     <FooterItem>
       <Title size="2">Thank you!</Title>
-      <Text>Really. For everyhing. We love you. We respect you. Hope to see you in the future.</Text>
+      <Text>
+        Really. For everything. We love you. We respect you. Hope to see you in
+        the future.
+      </Text>
     </FooterItem>
 
     <FooterItem>
